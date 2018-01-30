09b9f303c000dced2bc1da8520205b720f793018
PawelTo <pawel.toporowicz@gmail.com>
2018-01-30 01:30:26 +0100

    Adding Repository and Service layer with some basic methods

---
+++ FootballApplication/src/main/java/pl/pawel/service/MatchService.java
@@ -0,0 +1,30 @@
+package pl.pawel.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pl.pawel.model.Match;
+import pl.pawel.repository.MatchRepository;
+
+@Service
+public class MatchService {
+
+	//using Dependency Injection to get Repository layer
+	private final MatchRepository matchRepository;
+	
+	@Autowired
+	public MatchService(MatchRepository matchRepository) {
+		this.matchRepository = matchRepository;
+	}
+	
+	//*Consider pros and cons other kind of collections
+	public List<Match> findByHomeTeam(String homeTeam){
+		List<Match> listOfGames = new ArrayList<>();
+		for(Match game: matchRepository.findByHomeTeam(homeTeam))
+			listOfGames.add(game);
+		return listOfGames;
+	}
+}
