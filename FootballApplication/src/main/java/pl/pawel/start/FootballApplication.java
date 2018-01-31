0833d1b7e1fd1c4577dc07635f5237d47ec7484e
PawelTo <pawel.toporowicz@gmail.com>
2018-01-31 01:41:48 +0100

    Made changes:
    1. Adding necessary annotations (@EnityScan, @EnableJpaRepositories) to
    start class.
    2. Adding @Repository annotation to Match repository
    3. Creating draft of Service layer.

--- FootballApplication/src/main/java/pl/pawel/start/FootballApplication.java
+++ FootballApplication/src/main/java/pl/pawel/start/FootballApplication.java
@@ -2,10 +2,14 @@ package pl.pawel.start;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("pl.pawel")
+@EntityScan("pl.pawel")
+@EnableJpaRepositories("pl.pawel")
 public class FootballApplication {
 
 	public static void main(String[] args) {
