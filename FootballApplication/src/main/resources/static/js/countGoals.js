5204587bf6246330fba1b8f108f6b2bf793bf3fe
PawelTo <pawel.toporowicz@gmail.com>
2018-04-22 19:21:48 +0200

    Implementation of javascript feature to find game with the highest score

---
+++ FootballApplication/src/main/resources/static/js/countGoals.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+function getTheBiggestScore() {
+    document.getElementById("napis").innerHTML = "Paragraph changed.";
+}
+
+function openNewWindow(par){
+	window.open(par);
+}
+
+function getDataFromHTML() {
+	//method to prepare data from HTML to count the highest score
+	var dataFromHTML = Array.prototype.slice.call(arguments);
+	dataFromHTML = dataFromHTML.join();
+	var arrayAfterSpliting = dataFromHTML.split("Match");
+	var matchDetailsArray = [];
+	for(i=1; i<arrayAfterSpliting.length; i++){
+		matchDetailsArray[i-1]= arrayAfterSpliting[i].split(", ");
+		console.log(matchDetailsArray[i-1]);
+		matchDetailsArray[i-1][0] = parseInt(matchDetailsArray[i-1][0].substring(matchDetailsArray[i-1][0].search("=")+1));
+		matchDetailsArray[i-1][4] = parseInt(matchDetailsArray[i-1][4].substring(matchDetailsArray[i-1][4].search("=")+1));
+		matchDetailsArray[i-1][5] = parseInt(matchDetailsArray[i-1][5].substring(matchDetailsArray[i-1][5].search("=")+1));
+	}
+    document.getElementById("description").innerHTML = getMax(matchDetailsArray);
+}
+
+function getMax(parametr){
+	//method to count the highest score
+	var max = [0,-1];
+	console.log(parametr);
+	for(i=0;i<parametr.length;i++){
+		if(parametr[i][4]+parametr[i][5]>max[1]){
+			max[0] = parametr[i][0];
+			max[1] = parseInt(parametr[i][4])+parseInt(parametr[i][5]);
+		}
+	}
+    return "The biggest score id: " +max[0] + " number of goals: "+max[1];
+}
--- FootballApplication/src/main/resources/static/js/testing.js
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-function myFunction() {
-    document.getElementById("napis").innerHTML = "Paragraph changed.";
-}
-function myFunction2() {
-    document.write = "zmiana"
-}
