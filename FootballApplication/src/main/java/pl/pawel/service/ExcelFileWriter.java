ecd432db60e4c8c4311c039e96769cf3b29e0568
PawelTo <pawel.toporowicz@gmail.com>
2018-06-29 00:37:50 +0200

    Preparing formating of Excel Cells

--- FootballApplication/src/main/java/pl/pawel/service/ExcelFileWriter.java
+++ FootballApplication/src/main/java/pl/pawel/service/ExcelFileWriter.java
@@ -3,9 +3,14 @@ package pl.pawel.service;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Date;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -19,22 +24,35 @@ public class ExcelFileWriter implements IFileSaver{
 	private String fileName = "CreatedFile.xlsx";
 	private String sheetName = "Resuts of Games";
 	private Workbook workbook;
+	private CreationHelper creationHelper;
 	
 	@Override
 	public void saveMatch(Match game) {
 		String headers [] = {"Home Team","Away Team","Result"};
 		
 		workbook = new XSSFWorkbook();
+		creationHelper = workbook.getCreationHelper();
+		
+		CellStyle dataCell = workbook.createCellStyle();
+		dataCell.setDataFormat(creationHelper.createDataFormat().getFormat("DD-MM-YYYY"));
 		
 		Sheet sheet = workbook.createSheet(sheetName);
+		
 		Row headerRow = sheet.createRow(0);
 		for(int i=0;i<headers.length;i++) {
-			headerRow.createCell(i).setCellValue(headers[i]);
+			Cell cell = headerRow.createCell(i);
+			cell.setCellValue(headers[i]);
+			cell.setCellStyle(this.createStyle());
 		}
 		Row rowsWithData = sheet.createRow(1);
 		rowsWithData.createCell(0).setCellValue(game.getHomeTeam().getClubName());
 		rowsWithData.createCell(1).setCellValue(game.getAwayTeam().getClubName());
 		rowsWithData.createCell(2).setCellValue(game.getHomeTeamScore()+":"+game.getAwayTeamScore());
+		Cell randomData = rowsWithData.createCell(3);
+		randomData.setCellValue(Date.valueOf("2018-05-12"));
+		randomData.setCellStyle(dataCell);
+		rowsWithData.createCell(4).setCellValue("13.05.2018");
+		sheet.autoSizeColumn(1);
 		
 		try {
 			FileOutputStream fileOut = new FileOutputStream(path+fileName);
@@ -53,6 +71,12 @@ public class ExcelFileWriter implements IFileSaver{
 	private CellStyle createStyle() {
 		CellStyle cs = workbook.createCellStyle();
 		
+		//Font
+		Font headerFont = workbook.createFont();
+		headerFont.setBold(true);
+		headerFont.setColor(IndexedColors.AQUA.getIndex());
+		
+		cs.setFont(headerFont);
 		return cs;
 	}
 	
