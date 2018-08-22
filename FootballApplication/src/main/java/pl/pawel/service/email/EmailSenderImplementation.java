6fffeb19661871a5c86219af694298ef0d5a92f2
PawelTo <pawel.toporowicz@gmail.com>
2018-08-23 01:12:23 +0200

    Preparing new feature for sending emails

---
+++ FootballApplication/src/main/java/pl/pawel/service/email/EmailSenderImplementation.java
@@ -0,0 +1,34 @@
+package pl.pawel.service.email;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailSenderImplementation implements EmailSender{
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+	
+	@Override
+	public void sendEmail(String to, String subject, String text) {
+		MimeMessage mail = javaMailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+			helper.setTo(to);
+			helper.setSubject(subject);
+			helper.setText(text);
+			//some emails like gmail doesn't need to setFrom explicitly
+			helper.setFrom("xxx@o2.pl");
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		javaMailSender.send(mail);
+	}
+
+}
