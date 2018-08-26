d28b49da4ff2e6f108bcd16912cc0989ad37d866
PawelTo <pawel.toporowicz@gmail.com>
2018-08-26 23:40:40 +0200

    Adding feature to send email with attachment

--- FootballApplication/src/main/java/pl/pawel/service/email/EmailSenderImplementation.java
+++ FootballApplication/src/main/java/pl/pawel/service/email/EmailSenderImplementation.java
@@ -4,6 +4,7 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -39,6 +40,26 @@ public class EmailSenderImplementation implements EmailSender{
 			helper.setTo(email.getAddress());
 			helper.setSubject(email.getSubject());
 			helper.setText(email.getMessage());
+			//some emails like gmail doesn't need to setFrom explicitly
+			helper.setFrom("spring.tutorial@o2.pl");
+		} catch (MessagingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		javaMailSender.send(mail);
+	}
+	
+	@Override
+	public void sendEmailWithAttachment(Email email, String pathToAttachment) {
+		MimeMessage mail = javaMailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+			helper.setTo(email.getAddress());
+			helper.setSubject(email.getSubject());
+			helper.setText(email.getMessage());
+			FileSystemResource file = new FileSystemResource(pathToAttachment);
+			String fileFormat = file.getFilename().substring(file.getFilename().lastIndexOf("."));
+			helper.addAttachment("Nazwa_Zalacznika"+fileFormat, file);
 			//some emails like gmail doesn't need to setFrom explicitly
 			helper.setFrom("spring.tutorial@o2.pl");
 		} catch (MessagingException e) {
