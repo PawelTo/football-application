a321253b7f0d3478c84a7fd5a9fa3945391c505a
PawelTo <pawel.toporowicz@gmail.com>
2018-08-31 00:45:13 +0200

    Add feature to upload File

---
+++ FootballApplication/src/main/java/pl/pawel/service/fileUpload/StorageException.java
@@ -0,0 +1,12 @@
+package pl.pawel.service.fileUpload;
+
+public class StorageException extends RuntimeException{
+
+	public StorageException(String message) {
+		super(message);
+	}
+	
+	public StorageException(String message, Throwable cause) {
+		super(message,cause);
+	}
+}
--- FootballApplication/src/main/java/pl/pawel/start/FootballApplication.java
+++ FootballApplication/src/main/java/pl/pawel/start/FootballApplication.java
@@ -1,18 +1,33 @@
 package pl.pawel.start;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import pl.pawel.service.fileUpload.StorageProperties;
+import pl.pawel.service.fileUpload.StorageService;
 
 @SpringBootApplication
 @ComponentScan("pl.pawel")
 @EntityScan("pl.pawel")
 @EnableJpaRepositories("pl.pawel")
+@EnableConfigurationProperties(StorageProperties.class)
 public class FootballApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(FootballApplication.class, args);
-	}	
+	}
+	
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
