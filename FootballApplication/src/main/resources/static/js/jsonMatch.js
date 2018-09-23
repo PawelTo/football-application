19de61b03f7633a604feee210d39f77755529879
PawelTo <pawel.toporowicz@gmail.com>
2018-09-24 01:00:43 +0200

    Feature to go to previous match

--- FootballApplication/src/main/resources/static/js/jsonMatch.js
+++ FootballApplication/src/main/resources/static/js/jsonMatch.js
@@ -13,7 +13,10 @@ $(document).ready(function() {
 });
 
 const nextButton = document.querySelector('#nextButton');
-let nextMatchId = 5;
+const previousButton = document.querySelector('#previousButton');
+let nextMatchId = 1;
+let previousMatchId =-1;
+
 function goToNextMatch(){
 	$.ajax({
         url: "http://localhost:8080/testSPAMatch?id="+nextMatchId
@@ -22,11 +25,30 @@ function goToNextMatch(){
        $('.away-team').html("Away Team Score: "+data.awayTeamScore);
        $('.home-team-score').html("Attendance: "+data.attendance);
     });
+	
 	nextMatchId = nextMatchId+1;
 	nextButton.innerText = "Go to match: "+nextMatchId;
+	previousMatchId = previousMatchId+1;
+	previousButton.innerText = "Go to match: "+previousMatchId;
+}
+
+function goToPreviousMatch(){
+	$.ajax({
+        url: "http://localhost:8080/testSPAMatch?id="+previousMatchId
+    }).then(function(data) {
+       $('.home-team').html("Home Team Score: "+data.homeTeamScore);
+       $('.away-team').html("Away Team Score: "+data.awayTeamScore);
+       $('.home-team-score').html("Attendance: "+data.attendance);
+    });
+	
+	nextMatchId = nextMatchId-1;
+	nextButton.innerText = "Go to match: "+nextMatchId;
+	previousMatchId = previousMatchId-1;
+	previousButton.innerText = "Go to match: "+previousMatchId;
 }
 
 //adding events to objects
 document.addEventListener("DOMContentLoaded",function(){
 	nextButton.addEventListener('click', goToNextMatch);
+	previousButton.addEventListener('click', goToPreviousMatch);
 });
