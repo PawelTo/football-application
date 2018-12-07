caabbd28ea217f5ddaf363ef85cc9778318e0da6
PawelTo <pawel.toporowicz@gmail.com>
2018-12-07 02:25:53 +0100

    Adding Spy object to my tests.

--- FootballApplication/src/test/java/pl/pawel/service/competitionPointsCount/CompetitionPointsCountTestInjectMock.java
+++ FootballApplication/src/test/java/pl/pawel/service/competitionPointsCount/CompetitionPointsCountTestInjectMock.java
@@ -14,6 +14,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import pl.pawel.model.Club;
@@ -28,6 +30,18 @@ import pl.pawel.repository.MatchRepository;
 @RunWith(MockitoJUnitRunner.class)
 public class CompetitionPointsCountTestInjectMock {
 
+	@InjectMocks
+	private CompetitionPointsCount cmpCount;
+	
+	@Mock
+	private MatchRepository matchRepoMock;
+	
+	@Mock
+	private Club mockClub;
+	
+	@Spy
+	private Club spyClub = new Club("sClub", "spyStadiumName", 341);
+	
 	List<Match> oneMatchList;
 	List<Match> twoMatchList;
 	Club club1 = new Club("C1", "stadium1", 123);
@@ -44,16 +58,10 @@ public class CompetitionPointsCountTestInjectMock {
 		twoMatchList = new ArrayList<>();
 		twoMatchList.addAll(oneMatchList);
 		twoMatchList.add(m2);
-	}
-	
-	@InjectMocks
-	private CompetitionPointsCount cmpCount;
-	
-	@Mock
-	private MatchRepository matchRepoMock;
+	}	
 	
 	@Test
-	public void mockTest() {
+	public void getTableOfCompetitionTest_1Match_2Teams_MockInjection() {
 		LeagueTableRow team1 = new LeagueTableRow("C1",3,2,1);
 		LeagueTableRow team2 = new LeagueTableRow("C2",0,1,2);
 		
@@ -68,7 +76,7 @@ public class CompetitionPointsCountTestInjectMock {
 	}
 	
 	@Test
-	public void mockTest2() {
+	public void getTableOfCompetitionTest_2Match_3Teams_MockInjection() {
 		LeagueTableRow team1 = new LeagueTableRow("C1",4,3,2);
 		LeagueTableRow team2 = new LeagueTableRow("C2",0,1,2);
 		LeagueTableRow team3 = new LeagueTableRow("C3",1,1,1);
@@ -84,5 +92,56 @@ public class CompetitionPointsCountTestInjectMock {
 		assertEquals(expectedTable, actualTable);
 		verify(matchRepoMock,times(1)).findByCompetition(competition1);
 	}
+	
+	/**
+	 * Method to testing mocking object
+	 */
+	@Test
+	public void testMockingClub() {
+		//Changing name of mocking Club, but real name in table still should be null
+		mockClub.setClubName("mockClub");
+		Match m3 = new Match(LocalDate.of(2018, 12, 07), mockClub, club2, 2, 1, competition1, 123);
+		twoMatchList.add(m3);
+		
+		LeagueTableRow team1 = new LeagueTableRow("C1",4,3,2);
+		LeagueTableRow team2 = new LeagueTableRow("C2",0,2,4);
+		LeagueTableRow team3 = new LeagueTableRow("C3",1,1,1);
+		LeagueTableRow teamMock = new LeagueTableRow(null,3,2,1);
 
+		
+		List<LeagueTableRow> expectedTable = new ArrayList<>();
+		expectedTable.add(team1);
+		expectedTable.add(teamMock);
+		expectedTable.add(team3);
+		expectedTable.add(team2);
+		
+		List<LeagueTableRow> actualTable = cmpCount.getTableOfCompetition(twoMatchList);
+		assertEquals(expectedTable, actualTable);
+	}
+	
+	/**
+	 * Method to testing spying object
+	 */
+	@Test
+	public void testSpyingClub() {
+		//Changing name of spying Club - real name in table should also change
+		spyClub.setClubName("spyClub");
+		Match m3 = new Match(LocalDate.of(2018, 12, 07), spyClub, club2, 2, 1, competition1, 123);
+		twoMatchList.add(m3);
+		
+		LeagueTableRow team1 = new LeagueTableRow("C1",4,3,2);
+		LeagueTableRow team2 = new LeagueTableRow("C2",0,2,4);
+		LeagueTableRow team3 = new LeagueTableRow("C3",1,1,1);
+		LeagueTableRow teamSpy = new LeagueTableRow("spyClub",3,2,1);
+
+		
+		List<LeagueTableRow> expectedTable = new ArrayList<>();
+		expectedTable.add(team1);
+		expectedTable.add(teamSpy);
+		expectedTable.add(team3);
+		expectedTable.add(team2);
+		
+		List<LeagueTableRow> actualTable = cmpCount.getTableOfCompetition(twoMatchList);
+		assertEquals(expectedTable, actualTable);
+	}
 }
