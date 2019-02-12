13f409d870c17590c4b46c6b5f7a2ac59fab3439
PawelTo <pawel.toporowicz@gmail.com>
2019-02-13 00:33:23 +0100

    Create user registration for security reason

--- FootballApplication/src/main/java/pl/pawel/controller/MainController.java
+++ FootballApplication/src/main/java/pl/pawel/controller/MainController.java
@@ -1,10 +1,19 @@
 package pl.pawel.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.pawel.service.security.UserService;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping("/start")
 	public String startPage() {
@@ -25,5 +34,19 @@ public class MainController {
 	@RequestMapping("/accessDenied")
 	public String accessDeniedPage() {
 		return "accessDenied";
+	}
+	
+	@RequestMapping("/register")
+	public String createNewUser() {
+		return "register";
+	}
+	
+	@PostMapping("/createUser")
+	public String afterUserCreation(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String confirmedPassword = request.getParameter("confirm_password");
+		userService.createUser(username, password, confirmedPassword);
+		return "login";
 	}
 }
