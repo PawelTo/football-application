03f22be2862543a59a4576572fdfde8f9525101c
PawelTo <pawel.toporowicz@gmail.com>
2019-02-14 00:47:49 +0100

    Adding Logout feature

--- FootballApplication/src/main/java/pl/pawel/start/SecurityConfiguration.java
+++ FootballApplication/src/main/java/pl/pawel/start/SecurityConfiguration.java
@@ -3,21 +3,14 @@ package pl.pawel.start;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-import pl.pawel.service.security.UserService;
 
 @Configuration
 @EnableWebSecurity
@@ -46,7 +39,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		.antMatchers("/addNew").hasRole("ADMIN")
 		.antMatchers("/*").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
 		.anyRequest().authenticated() 
-		.and().formLogin().loginPage("/login").failureUrl("/login")
+		.and().formLogin().loginPage("/login").failureUrl("/login").defaultSuccessUrl("/start")
+		.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/register")
 		.and().exceptionHandling().accessDeniedPage("/accessDenied")
 		;
 	}
